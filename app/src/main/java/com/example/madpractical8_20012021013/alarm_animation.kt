676ff804c84cf3fe8acf266b70646815717ecb28
Arsh3102 <mahammadarshmansuri20@gnu.ac.in>
2022-11-18 14:33:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madpractical8_20012021013/alarm_animation.kt
@@ -0,0 +1,11 @@
+package com.example.madpractical8_20012021013
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class alarm_animation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm_animation)
+    }
+}
